2f8ac2f56d3422f650ce4fb6f657e4e9461e4232
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-01-08 20:17:55 -0600

    Chapter 1

---
+++ Quiz/ViewController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using UIKit;
+
+namespace Quiz
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		readonly string[] questions = new string[]
+		{
+			"What is 7+7?",
+			"What is the capital of Vermont?",
+			"What is cognac made from?"
+		};
+
+		readonly string[] answers = new string[]
+		{
+			"14",
+			"Montpelier",
+			"Grapes"
+		};
+
+		int currentQuestionIndex = 0;
+
+		partial void ShowNextQuestion(Foundation.NSObject sender)
+		{
+			currentQuestionIndex += 1;
+			if (currentQuestionIndex == questions.Length)
+			{
+				currentQuestionIndex = 0;
+			}
+			var question = questions[currentQuestionIndex];
+			QuestionLabel.Text = question;
+			AnswerLabel.Text = "???";
+		}
+
+		partial void ShowAnswer(Foundation.NSObject sender)
+		{
+			var answer = answers[currentQuestionIndex];
+			AnswerLabel.Text = answer;
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			QuestionLabel.Text = questions[currentQuestionIndex];
+		}
+	}
+}
---
+++ Quiz/ViewController.designer.cs
@@ -0,0 +1,40 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Quiz
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UILabel AnswerLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel QuestionLabel { get; set; }
+
+		[Action ("ShowAnswer:")]
+		partial void ShowAnswer (Foundation.NSObject sender);
+
+		[Action ("ShowNextQuestion:")]
+		partial void ShowNextQuestion (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (QuestionLabel != null) {
+				QuestionLabel.Dispose ();
+				QuestionLabel = null;
+			}
+
+			if (AnswerLabel != null) {
+				AnswerLabel.Dispose ();
+				AnswerLabel = null;
+			}
+		}
+	}
+}
