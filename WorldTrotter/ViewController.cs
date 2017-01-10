a153a7a2ebaee31e67f595d4c9d352c996bdd3d9
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-01-09 21:20:47 -0600

    WorldTrotter

---
+++ WorldTrotter/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace WorldTrotter
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			var firstFrame = new CGRect(160, 240, 100, 150);
+			var firstView = new UIView(firstFrame);
+			firstView.BackgroundColor = UIColor.Blue;
+			View.AddSubview(firstView);
+
+			var secondFrame = new CGRect(10, 30, 50, 50);
+			var secondView = new UIView(secondFrame);
+			secondView.BackgroundColor = UIColor.Green;
+			View.AddSubview(secondView);
+		}
+	}
+}
