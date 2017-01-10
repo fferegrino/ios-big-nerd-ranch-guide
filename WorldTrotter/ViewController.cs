8d18a0d70645a2c7aa87338f1f39b2e286456a1c
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-01-09 21:21:35 -0600

    Cleaning

--- WorldTrotter/ViewController.cs
+++ WorldTrotter/ViewController.cs
@@ -10,20 +10,5 @@ namespace WorldTrotter
 		{
 			// Note: this .ctor should not contain any initialization logic.
 		}
-
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-
-			var firstFrame = new CGRect(160, 240, 100, 150);
-			var firstView = new UIView(firstFrame);
-			firstView.BackgroundColor = UIColor.Blue;
-			View.AddSubview(firstView);
-
-			var secondFrame = new CGRect(10, 30, 50, 50);
-			var secondView = new UIView(secondFrame);
-			secondView.BackgroundColor = UIColor.Green;
-			firstView.AddSubview(secondView);
-		}
 	}
 }
