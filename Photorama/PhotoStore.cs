aebf8d2b1f3d54b2fb639c2a9c9d656d9f827137
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-03-23 20:36:00 -0600

    Photorama

---
+++ Photorama/PhotoStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
+using FlickrNet;
+using Foundation;
+
+namespace Photorama
+{
+    public class PhotoStore
+    {
+        private const string ApiKey = "186a848791f5d9a1fa0ed0d5c2736292";
+
+        public async Task FetchInterestingPhotos(Action<PhotoCollection> completion)
+        {
+            Flickr flickrApi = new Flickr(ApiKey);
+
+            var collection = await flickrApi.InterestingnessGetListAsync(perPage: 1);
+
+            completion?.Invoke(collection);
+        }
+    }
+}
---
+++ Photorama/PhotosViewController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FlickrNet;
+using Foundation;
+using UIKit;
+
+namespace Photorama
+{
+    [Register(nameof(PhotosViewController))]
+    public partial class PhotosViewController : UIViewController
+    {
+
+        [Outlet]
+        UIKit.UIImageView ImageView { get; set; }
+
+        public PhotoStore Store { get; set; }
+
+        public PhotosViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override async void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            await Store.FetchInterestingPhotos(Completion);
+        }
+
+        private async void Completion(PhotoCollection photoCollection)
+        {
+            var image = await FromUrl(photoCollection.First().ThumbnailUrl);
+            InvokeOnMainThread(() =>
+            {
+                ImageView.Image = image;
+            });
+
+
+        }
+
+        static async Task<UIImage> FromUrl(string uri)
+        {
+            UIImage img = null;
+            await Task.Run(() =>
+            {
+                using (var url = new NSUrl(uri))
+                using (var data = NSData.FromUrl(url))
+                    img = UIImage.LoadFromData(data);
+            });
+
+            return img;
+        }
+    }
+}
